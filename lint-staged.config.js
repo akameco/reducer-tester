f1237dbec18512e6a028c3be38e089043eab87f9
akameco <akameco.t@gmail.com>
2019-03-10 22:27:01 +0900

    chore(deps): upgrade deps

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  linters: {
+    '*.+(js|jsx|ts|tsx)': ['eslint --fix', 'jest --findRelatedTests'],
+    '*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx)': [
+      'prettier --write',
+      'git add',
+    ],
+  },
+}
